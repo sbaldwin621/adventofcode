691cf1dc71e21fb3533022380ddea4d347c130fd
Scott Baldwin <sbaldwin621@gmail.com>
2024-12-02 22:10:29 -0500

    Added makeshift 'part' option to CliOptions

--- 2024/day1/src/lib.rs
+++ 2024/day1/src/lib.rs
@@ -8,21 +8,23 @@ use thiserror::Error;
 
 #[derive(Parser)]
 pub struct CliOptions {
+    part: u32,
     filename: std::path::PathBuf
 }
 
 pub fn run(options: CliOptions) -> Result<u32, ApplicationError> {
+    let part = options.part;
     let filename = options.filename;
 
     let mut left_ids = vec![];
     let mut right_ids = vec![];
-    
+
     let lines = read_lines(filename)?;
     for line in lines {
         let line = line?;
         let mut split_iter = line.split_whitespace();
-        let left_id = split_iter.next().ok_or(ApplicationError::InvalidLine())?.parse::<u32>()?;
-        let right_id = split_iter.next().ok_or(ApplicationError::InvalidLine())?.parse::<u32>()?;
+        let left_id = split_iter.next().ok_or(ApplicationError::InvalidLine)?.parse::<u32>()?;
+        let right_id = split_iter.next().ok_or(ApplicationError::InvalidLine)?.parse::<u32>()?;
         
         left_ids.push(left_id);
         right_ids.push(right_id);
@@ -31,12 +33,24 @@ pub fn run(options: CliOptions) -> Result<u32, ApplicationError> {
     left_ids.sort();
     right_ids.sort();
 
+    match part {
+        1 => run_part1(left_ids, right_ids),
+        2 => run_part2(left_ids, right_ids),
+        _ => Err(ApplicationError::UnknownPart)
+    }
+}
+
+fn run_part1(left_ids: Vec<u32>, right_ids: Vec<u32>) -> Result<u32, ApplicationError> {
     let mut sum = 0;
     for (a, b) in zip(left_ids, right_ids) {
         sum += a.abs_diff(b);
     }
     
     Ok(sum)
+}
+
+fn run_part2(left_ids: Vec<u32>, right_ids: Vec<u32>) -> Result<u32, ApplicationError> {
+    unimplemented!()
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -47,10 +61,12 @@ where P: AsRef<Path>, {
 
 #[derive(Debug, Error)]
 pub enum ApplicationError {
+    #[error("unknown part")]
+    UnknownPart,
     #[error("couldn't read puzzle input: {0}")]
     CouldntReadInput(#[from] io::Error),
     #[error("invalid input line")]
-    InvalidLine(),
+    InvalidLine,
     #[error("invalid int: {0}")]
     InvalidInteger(#[from] std::num::ParseIntError)
 }
