a6451e408d0306437b827443b15a9cde9614dba9
Scott Baldwin <sbaldwin621@gmail.com>
2024-12-21 01:36:20 -0500

    Day 17 part 2 trying to see beyond the veil

--- 2024/day17/src/lib.rs
+++ 2024/day17/src/lib.rs
@@ -40,24 +40,138 @@ fn run_part1(debugger_info: &DebuggerInfo) -> Result<String, ApplicationError> {
     Ok(output.join(","))
 }
 
+
 fn run_part2(debugger_info: &DebuggerInfo) -> Result<String, ApplicationError> {
+    let original_length = debugger_info.original_program().len();
+
     // let mut low = 0;
     // let mut high = 10usize.pow(debugger_info.original_program().len().try_into().unwrap());
+    // 8.pow(program_length - 1)..8.pow(program_length)
+    //  35_184_372_088_832
+    // 281_474_976_710_655
 
-    for a in 1000000000000000.. {
+    // [
+    //     0o10000000_00000000,
+    //     0o10000000_00000000,
+    //     0o10000000_00000000,
+    //     0o20000000_00000000,
+    //     0o30000000_00000000,
+    //     0o40000000_00000000,
+    //     0o50000000_00000000,
+    //     0o60000000_00000000,
+    //     0o70000000_00000000,
+    //     0o77777777_77777777
+    // ]
+
+    println!("{}", 0o61120000_00000000usize - 0o61110000_00000000usize);
+
+    for n in [
+        0o61110000_00000000,
+        0o61110100_00000000,
+        0o61110200_00000000,
+        0o61110300_00000000,
+        0o61110400_00000000,
+        0o61110500_00000000,
+        0o61110600_00000000,
+        0o61110700_00000000,
+        
+        0o61111000_00000000,
+        0o61112000_00000000,
+        0o61113000_00000000,
+        0o61114000_00000000,
+        0o61115000_00000000,
+        0o61116000_00000000,
+        0o61117000_00000000
+    ] {
+        for guess in n.. {
+            let mut emulator = Emulator::from_debugger_info(debugger_info);
+            *emulator.register_a_mut() = guess;
+    
+            let mut a_values = vec![];
+            while emulator.step() {
+                a_values.push(emulator.register_a());
+            }
+    
+            let a_string: Vec<String> = a_values.iter().map(|v| v.to_string()).collect();
+            let a_string = a_string.join(",");
+    
+            let output = emulator.output_buffer();
+            let output_string = output.iter().map(|v| v.to_string()).collect::<Vec<String>>();
+            
+            println!("{}: {} ({})", guess, output_string.join(","), output.len());
+    
+            // println!("{}: {}", guess, a_string);
+    
+            // if output.len() < original_length {
+            //     low = guess + 1;
+            //     continue;
+            // } else if output.len() > original_length {
+            //     high = guess - 1;
+            //     continue;
+            // }
+    
+            // let mut match_count = 0;
+            // for (a, b) in debugger_info.original_program().iter().rev().zip(output.iter().rev()) {
+            //     if a == b {
+            //         match_count += 1;
+            //     } else {
+            //         break;
+            //     }
+            // }
+    
+            // if match_count == original_length {
+            //     return Ok(guess.to_string());
+            // }
+            break;
+        }
+    }
+    
+
+    unreachable!()
+}
+
+
+fn run_part2_a(debugger_info: &DebuggerInfo) -> Result<String, ApplicationError> {
+    let original_length = debugger_info.original_program().len();
+
+    let mut low = 0;
+    let mut high = 10usize.pow(debugger_info.original_program().len().try_into().unwrap());
+
+    loop {
+        let guess = (high - low) / 2;
+
         let mut emulator = Emulator::from_debugger_info(debugger_info);
-        *emulator.register_a_mut() = a;
+        *emulator.register_a_mut() = guess;
 
         while emulator.step() { }
 
         let output = emulator.output_buffer();
         let output_string = output.iter().map(|v| v.to_string()).collect::<Vec<String>>();
         
-        println!("{}: {}", a, output_string.join(","));
+        println!("{}: {}", guess, output_string.join(","));
 
-        if debugger_info.original_program().eq(output) {
-            return Ok(a.to_string());
+        if output.len() < original_length {
+            low = guess + 1;
+            continue;
+        } else if output.len() > original_length {
+            high = guess - 1;
+            continue;
         }
+
+        let mut match_count = 0;
+        for (a, b) in debugger_info.original_program().iter().rev().zip(output.iter().rev()) {
+            if a == b {
+                match_count += 1;
+            } else {
+                break;
+            }
+        }
+
+        if match_count == original_length {
+            return Ok(guess.to_string());
+        }
+
+        
     }
 
     unreachable!()
