95c79aa344456c97666adb778506e25eb5b86819
Scott Baldwin <sbaldwin621@gmail.com>
2024-12-22 17:11:00 -0500

    Day 21 starting point

---
+++ 2024/day21/src/keypad.rs
@@ -0,0 +1,89 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct PuzzleSolver {
+    codes: Vec<String>
+}
+
+#[derive(Debug)]
+pub struct KeypadSolver {
+
+}
+
+#[derive(Debug)]
+pub struct Keypad {
+    positions_to_keys: HashMap<Position, char>,
+    keys_to_positions: HashMap<char, Position>
+}
+
+impl Keypad {
+    pub fn numeric_keypad() -> Keypad {
+        let mut keys = HashMap::new();
+        
+        keys.insert(Position(0, 0), '7');
+        keys.insert(Position(1, 0), '8');
+        keys.insert(Position(2, 0), '9');
+
+        keys.insert(Position(0, 1), '4');
+        keys.insert(Position(1, 1), '5');
+        keys.insert(Position(2, 1), '6');
+
+        keys.insert(Position(0, 2), '1');
+        keys.insert(Position(1, 2), '2');
+        keys.insert(Position(2, 2), '3');
+
+        keys.insert(Position(1, 3), '0');
+        keys.insert(Position(2, 3), 'A');
+
+        Keypad::from_positions_to_keys(keys)
+    }
+
+    pub fn directional_keypad() -> Keypad {
+        let mut keys = HashMap::new();
+        
+        keys.insert(Position(1, 0), '^');
+        keys.insert(Position(2, 0), 'A');
+
+        keys.insert(Position(0, 1), '<');
+        keys.insert(Position(1, 1), 'v');
+        keys.insert(Position(2, 1), '>');
+
+        Keypad::from_positions_to_keys(keys)
+    }
+
+    fn from_positions_to_keys(positions_to_keys: HashMap<Position, char>) -> Keypad {
+        let keys_to_positions = positions_to_keys
+            .iter()
+            .map(|(&k, &v)| (v, k))
+            .collect();
+
+        Keypad { positions_to_keys, keys_to_positions }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West
+}
+
+static DIRECTIONS: &[Direction] = &[Direction::North, Direction::East, Direction::South, Direction::West];
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+pub struct Position(i32, i32);
+
+impl Position {
+    pub fn move_one(&self, direction: Direction) -> Position {
+        let x = self.0;
+        let y = self.1;
+
+        match direction {
+            Direction::North => Position(x, y - 1),
+            Direction::East => Position(x + 1, y),
+            Direction::South => Position(x, y + 1),
+            Direction::West => Position(x - 1, y)
+        }
+    }
+}
---
+++ 2024/day21/src/lib.rs
@@ -0,0 +1,45 @@
+use std::fs;
+use std::io;
+
+use clap::Parser;
+use thiserror::Error;
+
+mod keypad;
+
+#[derive(Parser)]
+pub struct CliOptions {
+    part: u32,
+    filename: std::path::PathBuf
+}
+
+pub fn run(options: CliOptions) -> Result<String, ApplicationError> {
+    let filename = options.filename;
+    let codes: Vec<String> = fs::read_to_string(filename)?
+        .lines()
+        .map(|s| s.trim().to_string())
+        .collect();
+    
+    let result = match options.part {
+        1 => run_part1(&codes),
+        2 => run_part2(),
+        _ => Err(ApplicationError::UnknownPart)
+    }?;
+    
+    Ok(result.to_string())
+}
+
+fn run_part1(codes: &Vec<String>) -> Result<u32, ApplicationError> {
+    todo!()
+}
+
+fn run_part2() -> Result<u32, ApplicationError> {
+    todo!()
+}
+
+#[derive(Debug, Error)]
+pub enum ApplicationError {
+    #[error("unknown part")]
+    UnknownPart,
+    #[error("couldn't read puzzle input: {0}")]
+    CouldntReadInput(#[from] io::Error)
+}
