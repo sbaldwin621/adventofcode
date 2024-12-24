f1772b73cc640897f7b4f07b6aefa4776b430225
Scott Baldwin <sbaldwin621@gmail.com>
2024-12-23 22:34:15 -0500

    Bitmask approach works on example

--- 2024/day23/src/network_map.rs
+++ 2024/day23/src/network_map.rs
@@ -35,38 +35,103 @@ impl NetworkMap {
         clusters
     }
 
+
     pub fn clusters(&self) -> () {
+        let mut node_to_id = HashMap::new();
+
+        let mut keys: Vec<_> = self.graph.keys().collect();
+        keys.sort();
+
+        if keys.len() > 63 {
+            panic!("keys can only be 63 long or less");
+        }
+
+        let mut n: u64 = 0;
+        for key in keys {
+            let id: u64 = 1 << n;
+            node_to_id.insert(key.to_string(), id);
+
+            n += 1;
+        }
+
+        let mut masks = HashMap::new();
+        for (node, neighbors) in self.graph.iter() {
+            let id = node_to_id[node];
+            let mut mask = id;
+
+            for neighbor in neighbors {
+                let neighbor_id = node_to_id[neighbor];
+
+                mask ^= neighbor_id;
+            }
+
+            masks.insert(node, mask);
+        }
+
+        let mut combined = HashMap::new();
+
+        for (&node, &mask) in masks.iter() {
+            for (_, &other_mask) in masks.iter() {
+                let anded = mask & other_mask;
+                if anded != 0 {
+                    *combined.entry(anded).or_insert(0_usize) += 1;
+                }
+            }
+        }
+
+        let highest_count = *combined.values().max().unwrap();
+        let most_common = combined.iter().filter(|(_, count)| **count == highest_count).next().unwrap().0;
+
+        let mut nodes_in_network = vec![];
+        for (node, &id) in node_to_id.iter() {
+            if id & most_common == id {
+                nodes_in_network.push(node);
+            }
+        }
+
+        nodes_in_network.sort();
+
+        println!("{:?}", nodes_in_network);
+
+        todo!()
+    }
+
+    fn clusters_(&self) -> () {
         // let mut clusters = HashSet::new();
 
         let mut node_queue: Vec<_> = self.graph.keys().collect();
-        let mut visited = HashSet::new();
+        println!("{}", node_queue.len());
 
         while let Some(node) = node_queue.pop() {
-            if !visited.insert(node) {
-                continue;
-            }
+            let mut visited = HashSet::new();
+            visited.insert(node);
 
             let mut cluster = HashSet::new();
             cluster.insert(node);
             
-            let mut walkers = vec![NodeWalker::new(node.clone())];
-            while !walkers.is_empty() {
-                println!("{}", walkers.len());
+            let mut walkers = HashSet::new();
+            walkers.insert(node);
 
-                let mut next_walkers = vec![];
+            while !walkers.is_empty() {
+                let mut next_walkers = HashSet::new();
 
                 for walker in walkers {
-                    let neighbors = self.graph.get(&walker.node).expect("node should always be in graph");
+                    let neighbors = self.graph.get(walker).expect("node should always be in graph");
                     for neighbor in neighbors {
-                        if !walker.path.contains(neighbor) {
-                            // println!("{}->{}", walker.node, neighbor);
-                            next_walkers.push(walker.add_node(neighbor.clone()));
+                        if visited.insert(neighbor) {
+                            let twobors = self.graph.get(neighbor).expect("neighbor should always be in graph");
+                            if twobors.contains(node) {
+                                cluster.insert(neighbor);
+                                next_walkers.insert(neighbor);
+                            }
                         }
                     }
                 }
 
                 walkers = next_walkers;
-            }        
+            }
+
+            println!("{} {:?}", node, cluster);
         }   
         
         todo!()
