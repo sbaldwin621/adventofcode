988bf0c1c0d985f6339a0cb13a6fb1bfd7f8136a
Scott Baldwin <sbaldwin621@gmail.com>
2025-07-03 19:23:31 -0400

    New template for starting 2015

---
+++ 2015/template/src/puzzle.rs
@@ -0,0 +1,19 @@
+use std::str::FromStr;
+
+use thiserror::Error;
+
+#[derive(Debug)]
+pub struct PuzzleInput {
+}
+
+impl FromStr for PuzzleInput {
+    type Err = ParsePuzzleInputError;
+    
+    fn from_str(_s: &str) -> Result<Self, Self::Err> {
+        todo!()
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum ParsePuzzleInputError {
+}
